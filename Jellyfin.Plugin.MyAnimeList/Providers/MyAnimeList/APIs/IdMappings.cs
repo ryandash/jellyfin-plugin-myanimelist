9772c6cd25fae21edc90d6a6adbdf9a855163cc8
ryandash <56605453+ryandash@users.noreply.github.com>
2026-03-05 16:01:59 -0500

    Update IdMappings.cs
    - Remove nullables

--- Jellyfin.Plugin.MyAnimeList/Providers/MyAnimeList/APIs/IdMappings.cs
+++ Jellyfin.Plugin.MyAnimeList/Providers/MyAnimeList/APIs/IdMappings.cs
@@ -15,7 +15,7 @@ namespace Jellyfin.Plugin.MyAnimeList.Providers.MyAnimeList.APIs
         //private readonly string movieUrl = "https://ryandash.github.io/TVDB-IDs-To-MyAnimeList-IDs/api/thetvdb-movie/{0}.json";
         private readonly HttpClient httpClient;
 
-        public IdMappings(HttpClient? client = null)
+        public IdMappings(HttpClient client = null)
         {
             httpClient = client ?? new HttpClient();
         }
@@ -29,16 +29,16 @@ namespace Jellyfin.Plugin.MyAnimeList.Providers.MyAnimeList.APIs
             public int? Episode { get; set; }
 
             [JsonPropertyName("thetvdb url")]
-            public string? TvdbUrl { get; set; }
+            public string TvdbUrl { get; set; }
 
             [JsonPropertyName("myanimelist url")]
-            public string? MalUrl { get; set; }
+            public string MalUrl { get; set; }
 
             [JsonPropertyName("myanimelist")]
             public long? MalId { get; set; }
 
             [JsonPropertyName("thetvdb")]
-            public string? Tvdb { get; set; }
+            public string Tvdb { get; set; }
         }
 
         private async Task<List<MappingEntry>> GetMappingsAsync(ILogger _log, string tvdbId)
@@ -65,7 +65,7 @@ namespace Jellyfin.Plugin.MyAnimeList.Providers.MyAnimeList.APIs
             }
         }
 
-        public async Task<AnimeEpisodeMapping?> GetAnimeEpisodeMappingAsync(ILogger _log, string tvdbId)
+        public async Task<AnimeEpisodeMapping> GetAnimeEpisodeMappingAsync(ILogger _log, string tvdbId)
         {
             var mappings = await GetMappingsAsync(_log, tvdbId);
             var entry = mappings.FirstOrDefault();
@@ -87,7 +87,7 @@ namespace Jellyfin.Plugin.MyAnimeList.Providers.MyAnimeList.APIs
     public class AnimeEpisodeMapping
     {
         public long? MalId { get; set; }
-        public string? MalUrl { get; set; }
+        public string MalUrl { get; set; }
         public int? Episode { get; set; }
         public int? Season { get; set; }
     }
